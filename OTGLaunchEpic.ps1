7a674ec865e58a4babc84a525084c591361c97e2
beemac88 <62079514+beemac88@users.noreply.github.com>
2024-12-16 13:57:36 -0800

    Update and rename OTGLaunchEpicV3.ps1 to OTGLaunchEpic.ps1
    Removed GzGameUserSettings.json portion which was tailored for a specific user.

--- OTGLaunchEpic.ps1
+++ OTGLaunchEpic.ps1
@@ -1,5 +1,5 @@
 # Define the URL of the latest script version on GitHub
-$scriptUrl = "https://raw.githubusercontent.com/beemac88/OTGCustomLaunch/main/OTGLaunchEpicV3.ps1"
+$scriptUrl = "https://raw.githubusercontent.com/beemac88/OTGCustomLaunch/main/OTGLaunchEpic.ps1"
 
 # Define the path to the current script
 $scriptPath = $MyInvocation.MyCommand.Path
@@ -19,52 +19,6 @@ try {
 } catch {
     Write-Output "An error occurred while trying to update the script: $_"
 }
-
-# Define the path to the JSON file
-$jsonFilePath = "$env:userprofile\Saved Games\OTG\GzGameUserSettings.json"
-
-# Desired ScreenResolution
-$desiredScreenResolution = @{
-    x = 3440
-    y = 1440
-}
-
-# Desired UIAspectRatio
-$desiredUIAspectRatio = 1.7777777910232544
-
-# Read the JSON file as a string
-$jsonContent = Get-Content -Path $jsonFilePath -Raw
-
-# Ensure the "WindowMode" value is added before "ScreenResolution"
-if ($jsonContent -notmatch '"WindowMode":\s*\d') {
-    $jsonContent = $jsonContent -replace '^\{', "{`r`n`t`"WindowMode`": 1,"
-} else {
-    $jsonContent = $jsonContent -replace '"WindowMode":\s*\d+', '"WindowMode": 1'
-}
-
-# Ensure the "ScreenResolution" value is added between "WindowMode" and "DisplayIndex"
-if ($jsonContent -notmatch '"ScreenResolution":\s*\{') {
-    $jsonContent = $jsonContent -replace '"WindowMode": 1', "`t`"WindowMode`": 1,`r`n`t`"ScreenResolution`":{`"x`": 3440,`"y`": 1440},"
-} else {
-    $jsonContent = $jsonContent -replace '"ScreenResolution":\s*\{\s*"x":\s*\d+,\s*"y":\s*\d+\s*\}', '"ScreenResolution":{"x": 3440,"y": 1440}'
-}
-
-# Ensure the "UIAspectRatio" value is added between "RotationAccelerationMultiplierHipFirePitch" and "bShowComparisonTooltip"
-if ($jsonContent -notmatch '"UIAspectRatio":\s*\d+\.\d+') {
-    $jsonContent = $jsonContent -replace '(("RotationAccelerationMultiplierHipFirePitch":\s*\d+\.\d+,))', "`$1`r`n`t`"UIAspectRatio`": 1.7777777910232544,"
-}
-
-# Ensure the "DisplayIndex" value is added after "ScreenResolution"
-if ($jsonContent -notmatch '"DisplayIndex":\s*\d') {
-    $jsonContent = $jsonContent -replace '}\s*,\s*"AntiAliasingQuality"', "},`r`n`t`"DisplayIndex`": 1,`r`n`t`"AntiAliasingQuality`""
-} else {
-    $jsonContent = $jsonContent -replace '"DisplayIndex":\s*\d+', '"DisplayIndex": 1'
-}
-
-# Write the updated JSON content back to the file without a trailing newline
-$jsonContent | Set-Content -Path $jsonFilePath -NoNewline
-
-Write-Output "The JSON file has been updated successfully."
 
 # Retrieve the DefaultAppInstallLocation from GameUserSettings.ini
 $defaultInstallLocation = Select-String -Path "$env:LOCALAPPDATA\EpicGamesLauncher\Saved\Config\Windows\GameUserSettings.ini" -Pattern "DefaultAppInstallLocation" | ForEach-Object { $_.Line.Split('=')[1].Trim() }
